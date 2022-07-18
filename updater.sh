6cfd04db2a4ad4d3590f87fdbcc2e5786eb9c9a2
dodiorne <33631789+dodiorne@users.noreply.github.com>
2022-07-18 13:43:54 -0400

    Create updater.sh

---
+++ updater.sh
@@ -0,0 +1,6 @@
+sudo apt-get update -y
+sudo apt-get dist-upgrade -y
+sudo apt install tesseract-ocr -y
+sudo apt install antiword -y
+sudo pip install pipx
+sudo pipx install git+https://github.com/blacklanternsecurity/MANSPIDER
