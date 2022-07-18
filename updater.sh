98f71641b7288d4b7faf1d9339cb52ea15fbbf01
dodiorne <33631789+dodiorne@users.noreply.github.com>
2022-07-18 14:24:52 -0400

    Update updater.sh

--- updater.sh
+++ updater.sh
@@ -6,7 +6,7 @@ sudo apt-get dist-upgrade -y
 sudo apt install tesseract-ocr -y
 sudo apt install antiword -y
 sudo pip install pipx
-sudo apt install python3.10-venvcd
+sudo apt install python3.10-venv
 sudo pipx install git+https://github.com/blacklanternsecurity/MANSPIDER
 pipx ensurepath
 
