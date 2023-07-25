c169a679beeda8b0ba8a7946446855edc973d6ca
dodiorne <33631789+dodiorne@users.noreply.github.com>
2023-07-25 16:56:47 -0400

    Update updater.sh
    tweaked the order of things and moved items not needed on every install to the bottom and commented them out.

--- updater.sh
+++ updater.sh
@@ -1,25 +1,23 @@
 ##update repositories
 sudo apt-get update -y
-sudo apt-get dist-upgrade -y
+sudo apt upgrade -y
+sudo apt dist-upgrade -y
 
 ##install manspider and dependecies
 sudo apt install tesseract-ocr -y
 sudo apt install antiword -y
-pipx ensurepath
 sudo apt install python3.11-venv
-sudo pip install pipx
 pip install pipx
 pipx install git+https://github.com/blacklanternsecurity/MANSPIDER
 
-
 ##install crackmapexec
-sudo apt-get install crackmapexec
+git clone https://github.com/mpgn/CrackMapExec
+cd CrackMapExec
+pipx install .
 
-##install responder
-sudo apt-get install responder
-
-##install john.smith wordlists
+##install john.smith wordlists and weak passwords list
 git clone https://github.com/dodiorne/john
+git clone https://github.com/dodiorne/lists
 
 ##install gowitness
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
@@ -27,7 +25,7 @@ sudo apt install ./google-chrome-stable_current_amd64.deb -y
 # google-chrome --no-sandbox
 sudo apt-get install golang -y
 go install github.com/sensepost/gowitness@latest
-export PATH=$PATH:~/go/bin/
+export PATH=$PATH:~/go/bin/:/home/kali/.local/bin
 
 ##install bloodhound.py
 git clone https://github.com/dodiorne/bloodhound.py
@@ -39,32 +37,30 @@ sudo apt install -y virtualbox-guest-x11
 sudo apt install remmina -y
 
 ##install hostapd-mana
-sudo apt install hostapd-mana -y
+#sudo apt install hostapd-mana -y
 
 ##initialize stuff
 sudo msfdb init
 
-##cleanup
-sudo updatedb
-sudo apt autoremove -y
-
-#Install Wireless Drivers for Alpha
-sudo apt install realtek-rtl88xxau-dkms -y
-sleep 60s
-sudo apt install dkms -y
-sleep 60s
-git clone https://github.com/aircrack-ng/rtl8812au 
-cd rtl8812au/ 
-make 
-sudo make install 
-lsusb 
-
-#install wifite pre-requs
-sudo apt install hcxtools -y
-sudo apt install hcxdumptool -y
-
-#install weak passwords list
-git clone https://github.com/dodiorne/lists
-
 #reboot the system
 sudo shutdown -r now
+
+##Removed Stuff
+##cleanup
+#sudo updatedb
+#sudo apt autoremove -y
+
+#Install Wireless Drivers for Alpha
+#sudo apt install realtek-rtl88xxau-dkms -y
+#sleep 60s
+#sudo apt install dkms -y
+#sleep 60s
+#git clone https://github.com/aircrack-ng/rtl8812au 
+#cd rtl8812au/ 
+#make 
+#sudo make install 
+#lsusb 
+
+#install wifite pre-requs
+#sudo apt install hcxtools -y
+#sudo apt install hcxdumptool -y
