b4f0c12e489a9108411e1a7a02c955e6e5d21b5a
dodiorne <33631789+dodiorne@users.noreply.github.com>
2023-08-25 10:00:40 -0400

    Update apt-updater.sh
    added file to continue installation after reboot

--- apt-updater.sh
+++ apt-updater.sh
@@ -8,5 +8,8 @@ sudo apt dist-upgrade -y
 ##create indicator file
 touch /home/kali/deploy/apt-updater-done.txt 
 
+##add file to continue installation after reboot
+sudo cp /home/kali/deploy/continue.sh /etc/init.d/continue.sh
+
 #reboot the system
 sudo shutdown -r now
